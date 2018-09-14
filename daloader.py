d21cd94377f99bcb9cc386bd31bf51a0bf33da00
Sebastian Riedel <git@basxto.de>
2018-09-14 09:22:20 +0200

    Filter adult content

--- daloader.py
+++ daloader.py
@@ -12,7 +12,7 @@ specialChars = re.compile('[^a-zA-Z0-9-]+')
 urlRegex = re.compile('https?://')
 rssLinks = re.compile('<guid isPermaLink="true">(.*?)</guid>')
 
-def downloadDeviation(url, cc_only):
+def downloadDeviation(url, ccOnly, sfwOnly, nsfwOnly):
     global ccRegex
     global ccVerRegex
     global specialChars
@@ -22,13 +22,19 @@ def downloadDeviation(url, cc_only):
         return
     # get json representation
     deviation = requests.get('https://backend.deviantart.com/oembed?url={}'.format(url)).json()
+    if sfwOnly and deviation['safety'] == 'adult':
+        sys.stderr.write('Skip adult content {}\n'.format(url))
+        return
+    if nsfwOnly and deviation['safety'] == 'nonadult':
+        sys.stderr.write('Skip non-adult content {}\n'.format(url))
+        return
     license = 'proprietary'
     licenseUrl = '#'
     if 'license' in deviation and '_attributes' in deviation['license'] and len(deviation['license']['_attributes']['href']) > 0:
         href = deviation['license']['_attributes']['href']
         license = ccRegex.findall(href)[0] + ' ' + ccVerRegex.findall(href)[0]
         licenseUrl = deviation['license']['_attributes']['href']
-    if license == 'proprietary' and cc_only:
+    if license == 'proprietary' and ccOnly:
         sys.stderr.write("Skip {} deviation {}\n".format(license, url))
     else:
         # replace all special characters with _
@@ -57,17 +63,19 @@ def main():
     parser.add_argument("--query", help="Download first 60 matches for term")
     parser.add_argument("--offset", default="0", help="Skip x first search entries")
     parser.add_argument("--cc-only", help="Only allow deviations licensed under creative commons")
+    parser.add_argument("--no-adult", help="Only allow deviations, which are not mature content")
+    parser.add_argument("--adult-only", help="Only allow adult deviations")
     args = parser.parse_args()
     if args.query:
         search = requests.get('https://backend.deviantart.com/rss.xml?type=deviation&q={}&offset={}'.format(args.query,args.offset)).text
         for url in rssLinks.findall(search):
-            downloadDeviation(url.strip(), args.cc_only)
+            downloadDeviation(url.strip(), args.cc_only, args.no_adult, args.adult_only)
     elif args.f:
         with open(args.f, 'r') as file:
             for url in file:
-                downloadDeviation(url.strip(), args.cc_only)
+                downloadDeviation(url.strip(), args.cc_only, args.no_adult, args.adult_only)
     elif args.url:
-        downloadDeviation(args.url.strip(), args.cc_only)
+        downloadDeviation(url.strip(), args.cc_only, args.no_adult, args.adult_only)
     else:
         parser.print_help()
 
